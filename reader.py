6418f7f79373bc21a5c5d30db621a17bfcacfed5
jcksndy <jcksnmdy1@gmail.com>
2021-11-15 23:54:58 -0500

    ehy

--- reader.py
+++ reader.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
 import pygame
 import random
 import time
@@ -8,77 +13,74 @@ import csv
 
 id = 1
 pygame.init()
- 
- 
-def checkRFidTag():
-    tagId = rfidText.value
-    print(tagId)
-    return tagId
- 
-print("Checking in 3")
-time.sleep(1)
-print("Checking in 2")
-time.sleep(1)
-print("Checking in 1")
-time.sleep(1)
 
-if id == checkRFidTag():
-    randomNum=random.randint(1, 10)
-    pygame.mixer.music.load("music/acoustic/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
+while True:
 
-if id == 2:
-    randomNum=random.randint(1, 11)
-    pygame.mixer.music.load("music/disney/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
-if id == 3:
-    randomNum=random.randint(1, 8)
-    pygame.mixer.music.load("music/faouzia/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
-if id == 4:
-    randomNum=random.randint(1, 11)
-    pygame.mixer.music.load("music/larenDaigle/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
+    reader = SimpleMFRC522()
+    try:
+            id, text = reader.read()
+            print(id)
+            print(text)
+    finally:
+        GPIO.cleanup()
 
-if id == 5:
-    randomNum=random.randint(1, 9)
-    pygame.mixer.music.load("music/niallHoran/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
+    if id == 1:
+        randomNum=random.randint(1, 10)
+        pygame.mixer.music.load("music/acoustic/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
 
-if id == 6:
-    randomNum=random.randint(1, 6)
-    pygame.mixer.music.load("music/latin/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
+    if id == 2:
+        randomNum=random.randint(1, 11)
+        pygame.mixer.music.load("music/disney/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
+    if id == 3:
+        randomNum=random.randint(1, 8)
+        pygame.mixer.music.load("music/faouzia/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
+    if id == 4:
+        randomNum=random.randint(1, 11)
+        pygame.mixer.music.load("music/larenDaigle/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
 
-if id == 7:
-    randomNum=random.randint(1, 7)
-    pygame.mixer.music.load("music/romance/" + str(randomNum) + ".mp3")
-    pygame.mixer.music.play(0)
-    while pygame.mixer.music.get_busy():
-        print("playing")
-        if (input()=="s"):
-            pygame.mixer.music.stop()
+    if id == 5:
+        randomNum=random.randint(1, 9)
+        pygame.mixer.music.load("music/niallHoran/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
+
+    if id == 6:
+        randomNum=random.randint(1, 6)
+        pygame.mixer.music.load("music/latin/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
+
+    if id == 7:
+        randomNum=random.randint(1, 8)
+        pygame.mixer.music.load("music/romance/" + str(randomNum) + ".mp3")
+        pygame.mixer.music.play(0)
+        while pygame.mixer.music.get_busy():
+            print("playing")
+            if (input()=="s"):
+                pygame.mixer.music.stop()
