ab068a3eb5fabe0c19f4f17d6bf406981e8be032
jcksndy <jcksnmdy1@gmail.com>
2021-11-28 02:27:41 -0500

    song

--- reader.py
+++ reader.py
@@ -16,45 +16,45 @@ while True:
         pygame.mixer.music.load("music/acoustic/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 2:
+    if id == "0003825451":
         randomNum=random.randint(1, 11)
         pygame.mixer.music.load("music/disney/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 3:
+    if id == "0004086613":
         randomNum=random.randint(1, 14)
         pygame.mixer.music.load("music/faouzia/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 4:
+    if id == "0004177330":
         randomNum=random.randint(1, 11)
         pygame.mixer.music.load("music/larenDaigle/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
 
-    if id == 5:
+    if id == "0003825420":
         randomNum=random.randint(1, 9)
         pygame.mixer.music.load("music/niallHoran/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 6:
+    if id == "0003825428":
         randomNum=random.randint(1, 6)
         pygame.mixer.music.load("music/latin/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 7:
+    if id == "0004086602":
         randomNum=random.randint(1, 14)
         pygame.mixer.music.load("music/romance/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 8:
+    if id == "0004177315":
         randomNum=random.randint(1, 9)
         pygame.mixer.music.load("music/christian/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
         
-    if id == 9:
+    if id == "0003825442":
         randomNum=random.randint(1, 20)
         pygame.mixer.music.load("music/pop/" + str(randomNum) + ".mp3")
         pygame.mixer.music.play(0)
 
-    if id == 10:
+    if id == "0004086591":
         os.system("sudo shutdown now")
